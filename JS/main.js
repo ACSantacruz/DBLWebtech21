7701d7dcf472cf2aa4922a856f070df37b7b1aac
Krinkel01 <70716327+Krinkel01@users.noreply.github.com>
2021-04-28 18:56:22 +0200

    Working table
    Finally got the table working took me too long even though it is quite simple when i am looking back at it.

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,19 @@
+import tableMaker from "./tableMaker.js";
+
 d3.select();
 d3.selectAll();
 
 d3.select('h1').style('color', 'blue');
+
+const tableRoot = document.querySelector("#tableData");
+const tableCsv = new tableMaker(tableRoot);
+
+csvUploader.addEventListener("change", (e) => {
+    Papa.parse(csvUploader.files[0], {
+        delimiter: ",",
+        skipEmptyLines: true,
+        complete: (results) => {
+            tableCsv.update(results.data.slice(1), results.data[0]);
+        }
+    });
+});
