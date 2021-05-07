1039e8a184e68b9925321857e32afed76a0d6149
Krinkel01 <70716327+Krinkel01@users.noreply.github.com>
2021-05-07 14:45:56 +0200

    PapaParse removed

--- JS/main.js
+++ JS/main.js
@@ -2,21 +2,3 @@ import tableMaker from "./tableMaker.js";
 
 const tableRoot = document.querySelector("#tableData");
 const tableCsv = new tableMaker(tableRoot);
-
-csvUploader.addEventListener("change", (e) => {
-    async function getData() {
-        const response = await fetch('csvUploader')
-        const data = await response.text();
-        console.log(data)
-    }
-});
-
-csvUploader.addEventListener("change", (e) => {
-    Papa.parse(csvUploader.files[0], {
-        delimiter: ",",
-        skipEmptyLines: true,
-        complete: (results) => {
-            tableCsv.update(results.data.slice(1), results.data[0]);
-        }
-    });
-});
