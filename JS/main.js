461e8d491d70a1a96202baa816301d0ef69c72ab
Krinkel01 <70716327+Krinkel01@users.noreply.github.com>
2021-05-11 23:22:15 +0200

    Heatmap + basic file info functionality
    Heatmap added, lot of "bugs" mainly visual and things to fix, however it should work accordingly :D

--- JS/main.js
+++ JS/main.js
@@ -15,8 +15,9 @@ function parseFile(){
 
         return d;
     });
+    fileInfo(data)
     createTable(data);
-    createLineGraph(data);
+    createHeatMap(data);
 }
 
 function createTable(data) {
@@ -76,6 +77,141 @@ function createTable(data) {
 }
 
 
-function createLineGraph(data) {
+function createHeatMap(data) {
+
+    // Using the standard Size thing from JS does anyone know how to convert this to scale to the size of the boxes>?
+    var margin = {top: 80, right: 25, bottom: 30, left: 40},
+        width = 600 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+
+    //Printing the field, still using the margin set above.
+    var svg = d3.select("#heatMap")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .style("background-color", "white")
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+        // There is an easier way to do this by using d.fromJobTitle and taking the keys, but then it has to be sorted to make sure they are in the same order.
+        // So I believe this is faster.
+        var fromTitle = ["Unknown", "Employee", "Trader", "In House Lawyer", "Manager", "Managing Director", "Director", "Vice President", "President", "CEO"]
+        var toTitle = ["Unknown", "Employee", "Trader", "In House Lawyer", "Manager", "Managing Director", "Director", "Vice President", "President", "CEO"]
+
+
+    //This is to make the x- axis and to make the grid layout scalable But does not work
+    var xAxis = d3.scaleBand()
+        .range([ 0, width ])
+        .domain(fromTitle)
+        .padding(0.03);
+
+    //This is to make the y- axis and to make the grid layout scalable But does not work
+        var yAxis = d3.scaleBand()
+            .range([ height, 0 ])
+            .domain(toTitle)
+            .padding(0.03);
+
+
+        //To colour in the heatmap, can someb
+        var ColourHM = d3.scaleLinear()
+            .range(["#ffffff", "#ff0000", "#0023ff"])
+            .domain([-0.07,0.07])
+
+        // For When the mouse goes on a square
+        var mouseHover = d3.select("#heatMap")
+            .append("div")
+            .style("opacity", 0)
+            .attr("class", "tooltip")
+
+            .style("background-color", "#b6b6b6")
+            .style("border", "solid")
+            .style("border-width", "3px")
+            .style("border-radius", "20px")
+            .style("padding", "10px")
+            .style("width", "300px")
+
+
+        //When the mouse is over the square.
+        var mouseOnSquare = function(d) {
+            mouseHover
+                .style("opacity", 1)
+            d3.select(this)
+                .style("stroke", "black")
+                .style("opacity", 1)
+        }
+
+
+        //Putting the text down
+        var textDisplay = function(d) {
+            mouseHover
+                .html("value: " + d.sentiment)
+        }
+
+
+        //Return it to the original form what it was before mouse hover
+        var mouseOffSquare = function(d) {
+            mouseHover
+                .style("opacity", 0)
+            d3.select(this)
+                .style("stroke", "none")
+                .style("opacity", 0.8)
+        }
+
+// A title for the visualization, maybe do this for all vis?
+    svg.append("text")
+        .style("font-size", "24px")
+        .style("font-family", "Verdana")
+        .text("Sentiment Heatmap");
+
+        //Adding the squares
+        svg.selectAll()
+            .data(data, function(d) {
+                return d.fromJobtitle+':'+d.toJobtitle;
+            })
+            .enter()
+
+            .append("rect")
+            .attr("x", function(d) {
+                return xAxis(d.fromJobtitle)
+            })
+            .attr("y", function(d) {
+                return yAxis(d.toJobtitle)
+            })
+
+            .attr("rx", 8)
+            .attr("ry", 8)
+
+            .style("stroke-width", 3)
+            .style("opacity", 0.9)
+
+            .attr("width", xAxis.bandwidth() )
+            .attr("height", yAxis.bandwidth() )
+
+            //Using the colour heatmap function made earlier to give the squares "heat"
+            .style("fill", function(d) {
+                return ColourHM(d.sentiment)
+            })
+
+            //d3 built in mouse interactivity stuff
+            .on("mouseover", mouseOnSquare)
+            .on("mousemove", textDisplay)
+            .on("mouseleave", mouseOffSquare);
+}
+
+function fileInfo(data){
+//Does not work for some reason?
+    if (d3.keys(data[0]) == ["date", "fromId", "fromEmail", "fromJobtitle", "toId", "toEmail", "toJobtitle", "messageType", "sentiment"] ) {
+
+        var svg = d3.select("#box3")
+        svg.append("text")
+            .text("The file is in the correct format."+ + d3.keys(data[0]));
+    } else {
+        console.log(d3.keys(data[0]) )
+        console.log(["date", "fromId", "fromEmail", "fromJobtitle", "toId", "toEmail", "toJobtitle", "messageType", "sentiment"])
+        var svg = d3.select("#box3")
+        svg.append("text")
+            .text("ERROR!! The file is not the correct format, this might cause issues!.");
+    }
 
 }
