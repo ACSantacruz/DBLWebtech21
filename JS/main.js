5350dc618ef43efe66332db679bfb984fba69d64
kelvinoss1 <k.tran1@student.tue.nl>
2021-06-01 19:42:35 +0200

    Start of the Negativity spread visualization

--- JS/main.js
+++ JS/main.js
@@ -19,6 +19,7 @@ function parseFile(){
     createTable(data);
     createHeatMap(data);
     createUniqueness(data);
+    createNegativity(data);
     //createUniqueGraph(data); unfinished
     //createUGTest(data);
 }
@@ -389,3 +390,78 @@ function fileInfo(data){
         }
     });
 } */
+
+function createNegativity(data) {
+    var margin = {top: 80, right: 25, bottom: 30, left: 40},
+        width = 600 - margin.left - margin.right,
+        height = 600 - margin.top - margin.bottom;
+
+
+    //Printing the field, still using the margin set above.
+    var svg = d3.select("#Negativity")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .style("background-color", "white")
+        .append("g")
+        .attr("viewBox", [0, 0, width, height]);
+        var width = 960,
+    height = 500;
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var force = d3.layout.force()
+    .size([width, height]);
+
+d3.csv("graph.csv", function(error, links) {
+  if (error) throw error;
+
+  var nodesByName = {};
+
+  // Create nodes for each unique source and target.
+  links.forEach(function(link) {
+    link.source = nodeByName(link.source);
+    link.target = nodeByName(link.target);
+  });
+
+  // Extract the array of nodes from the map by name.
+  var nodes = d3.values(nodesByName);
+
+  // Create the link lines.
+  var link = svg.selectAll(".link")
+      .data(links)
+    .enter().append("line")
+      .attr("class", "link");
+
+  // Create the node circles.
+  var node = svg.selectAll(".node")
+      .data(nodes)
+    .enter().append("circle")
+      .attr("class", "node")
+      .attr("r", 4.5)
+      .call(force.drag);
+
+  // Start the force layout.
+  force
+      .nodes(nodes)
+      .links(links)
+      .on("tick", tick)
+      .start();
+
+  function tick() {
+    link.attr("x1", function(d) { return d.source.x; })
+        .attr("y1", function(d) { return d.source.y; })
+        .attr("x2", function(d) { return d.target.x; })
+        .attr("y2", function(d) { return d.target.y; });
+
+    node.attr("cx", function(d) { return d.x; })
+        .attr("cy", function(d) { return d.y; });
+  }
+
+  function nodeByName(name) {
+    return nodesByName[name] || (nodesByName[name] = {name: name});
+  }
+});
+}
