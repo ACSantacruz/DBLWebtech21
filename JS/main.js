ec7e3131162312bb99c69c23fac42446d5622ea8
Coco <70581817+CocoCosmos@users.noreply.github.com>
2021-06-09 10:26:43 +0200

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -571,7 +571,7 @@ function createPieGraph(data) { //https://observablehq.com/@d3/donut-chart
         width = 600 - margin.left - margin.right,
         height = 600 - margin.top - margin.bottom;
 
-    var svg = d3.select("#heatMap")
+    var svg = d3.select("#pie")
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
